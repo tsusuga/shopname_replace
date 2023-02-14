517ba6a9f6776bdaf1254dc021e9c505422cf870
tsusuga <tsusuga@j-com.co.jp>
2023-02-14 15:39:41 +0900

    カウンタ設置

--- replace.py
+++ replace.py
@@ -3,12 +3,14 @@ import glob
 
 source_file = 'csv/source.csv'
 target_file = 'target/_shopdata.ejs'
+counter = 0
 
 with open(source_file, encoding="utf-8") as f:
   source_data = csv.reader(f)
   for row in source_data:
     before_name = row[1]
     after_name = row[0]
+
 
     with open(target_file, encoding="utf-8") as f:
       after_file = f.read()
@@ -18,6 +20,9 @@ with open(source_file, encoding="utf-8") as f:
       f.write(after_file)
       print("変更後：" +row[0])
       print("Success!====================")
+      counter += 1
 
 print('end')
+print(counter)
+
 
