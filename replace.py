2bf02a40a8a6315839ac15be667c3d56e28b0d30
tsusuga <tsusuga@j-com.co.jp>
2023-02-14 16:37:03 +0900

    微調整

--- replace.py
+++ replace.py
@@ -6,10 +6,10 @@ counter = 0
 
 with open(source_file, encoding="utf-8") as f:
   source_data = csv.reader(f)
+
   for row in source_data:
     before_name = row[1]
     after_name = row[0]
-
 
     with open(target_file, encoding="utf-8") as f:
       after_file = f.read()
@@ -17,8 +17,7 @@ with open(source_file, encoding="utf-8") as f:
 
     with open('target/after/after_change.txt', 'w', encoding="utf-8") as f:
       f.write(after_file)
-      print("変更後：" +row[0])
-      print("Success!====================")
+      print(row[0])
       counter += 1
 
 print('end')
