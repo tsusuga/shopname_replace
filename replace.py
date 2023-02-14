e4716d2597f871df9b92848cf515f2a4b79128a9
tsusuga <tsusuga@j-com.co.jp>
2023-02-14 15:10:48 +0900

    ほぼ完成

---
+++ replace.py
@@ -0,0 +1,21 @@
+import csv
+import glob
+
+source_file = 'csv/source.csv'
+target_file = 'target/_shopdata.txt'
+
+with open(source_file, encoding="utf-8") as f:
+  source_data = csv.reader(f)
+  for row in source_data:
+    before_name = row[1]
+    after_name = row[0]
+
+    with open(target_file, encoding="utf-8") as f:
+      after_file = f.read()
+      after_file = after_file.replace(before_name, after_name)
+
+    with open(target_file, 'w', encoding="utf-8") as f:
+      f.write(after_file)
+      print("変更後：" +row[0])
+      print("Success!====================")
+
