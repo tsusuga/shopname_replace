0a253a2ddb8dcdc859d8d3bd175f0fb016116eb1
tsusuga <tsusuga@j-com.co.jp>
2023-03-30 17:17:32 +0900

    出力ファイル名変更。しかし置換されない不具合あり

--- replace.py
+++ replace.py
@@ -14,13 +14,12 @@ with open(source_file, encoding="utf-8") as f:
     with open(target_file, encoding="utf-8") as f:
       after_file = f.read()
       after_file = after_file.replace(before_name, after_name)
+      # print(before_name, after_name)
 
-    with open('target/after/after_change.txt', 'w', encoding="utf-8") as f:
+    with open('target/after/_shopdata_changed.ejs', 'w', encoding="utf-8") as f:
       f.write(after_file)
       print(row[0])
       counter += 1
 
 print('end')
 print(counter)
-
-
